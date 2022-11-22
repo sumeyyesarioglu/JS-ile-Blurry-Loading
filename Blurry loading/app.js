eafc4349c70fcb9a02716ba46cf9a57500b62add
sumeyyesarioglu <101519570+sumeyyesarioglu@users.noreply.github.com>
2022-11-22 23:49:42 +0300

    Blurry Loading
    Blurry Loading

---
+++ Blurry loading/app.js
@@ -0,0 +1,22 @@
+const bg = document.querySelector('.bg')
+const loadText = document.querySelector('.loading-text')
+
+let load = 0
+
+let int=  setInterval(blurring,30);//30 saniyede bir yazılmasını sağla
+
+
+function blurring () {
+    load++
+    if(load>99) {
+        clearInterval(int)//temiz bir sekilde yazılımı sağlıyor 100'e kadar
+    }
+
+    loadText.innerText= `${load}%`
+    loadText.style.opacity =scale(load, 0, 100, 1, 0 )//Görünmezden resmi netleştirmeye doğru gittiğimiz için 1, 0 yaptık tam tersi olsaydı eğer 0 ve 1 olacaktı. 
+    bg.style.filter= `blur(${scale(load, 0 ,100, 30, 0)}px)` //Bununlada fotoğrafı bulanıktan nete doğru gitmesini sağladık
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+}
